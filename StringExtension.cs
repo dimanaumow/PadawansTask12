2a2d70f0ba829485a81c3b6fd66ba06dffb2f1c0
Dima Naumow <dimanaumow123@gmail.com>
2019-02-28 12:18:16 +0300

    first attempts

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,7 +6,28 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            if(source == null)
+            {
+                throw new ArgumentNullException(nameof(source)); 
+            }
+
+            if(source.Length == 0)
+            {
+                throw new ArgumentException(); 
+            }
+
+            for(int i = 0; i < source.Length; i++)
+            {
+                for(int j = 0; j < source.Length; j++)
+                {
+                    if(source[i] == source[j] && i != j)
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            return true; 
         }
     }
 }
